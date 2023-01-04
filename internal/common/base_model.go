b7768d027168f7a3957162e98fbdba8d4bbeadae
viettranvt <enzoferrari.vt@gmail.com>
2023-01-04 20:45:58 +0700

    MOD update model

--- internal/common/base_model.go
+++ internal/common/base_model.go
@@ -10,5 +10,5 @@ type BaseModel struct {
 	ID        primitive.ObjectID `bson:"_id" json:"id"`
 	CreatedAt time.Time          `bson:"created_at" json:"created_at"`
 	UpdatedAt time.Time          `bson:"updated_at" json:"updated_at"`
-	DeletedAt time.Time          `bson:"deleted_at,omitempty" json:"deleted_at,omitempty"`
+	DeletedAt time.Time          `bson:"deleted_at,omitempty" json:"-"`
 }
