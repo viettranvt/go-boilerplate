f856751582e8812dc7945c5ce21ac1d4f7d23710
viettranvt <enzoferrari.vt@gmail.com>
2023-04-03 09:38:48 +0700

    ADD update cors

--- internal/configs/gin/middleware/secure.go
+++ internal/configs/gin/middleware/secure.go
@@ -4,10 +4,9 @@ import (
 	"net/http"
 	"os"
 	"parishioner_management/internal/components"
+	configs "parishioner_management/internal/configs"
 	options_util "parishioner_management/internal/utils/options"
 	"strings"
-
-	configs "parishioner_management/internal/configs"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -15,10 +14,12 @@ import (
 
 func secureMiddleware(g *gin.Engine, appCtx components.AppContext, options options_util.Options) {
 	origin := os.Getenv(configs.EnvCorsOrigin)
-
-	g.Use(cors.New(cors.Config{
+	config := cors.New(cors.Config{
 		AllowCredentials: true,
+		AllowHeaders:     []string{"Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With"},
 		AllowOrigins:     strings.Split(origin, ","),
-		AllowMethods:     []string{http.MethodGet, http.MethodHead, http.MethodPut, http.MethodPatch, http.MethodPost, http.MethodDelete},
-	}))
+		AllowMethods:     []string{http.MethodGet, http.MethodHead, http.MethodPut, http.MethodPatch, http.MethodPost, http.MethodDelete, http.MethodOptions},
+	})
+
+	g.Use(config)
 }
